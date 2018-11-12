ce83100e709d437aee230dee0d95211a83537560
Roland Arnoldt <rolandarnoldt@10-17-244-250.dynapool.wireless.nyu.edu>
2018-11-12 08:40:51 -0500

    major cleanup

--- utils.js
+++ utils.js
@@ -1,3 +1,12 @@
+////////////////////////////////////////////////////////////////////////
+// rewrite and modification of tensorflow person-segmentation masking//
+//////////////////////////////////////////////////////////////////////
+
+// major code sources:
+// *https://github.com/tensorflow/tfjs-models/tree/master/person-segmentation
+// *https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Manipulating_video_using_canvas
+
+
 var outputStride = 16;
 var flipHorizontal = false;
 var segmentationThreshold = 0.5;
@@ -33,112 +42,32 @@ navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
     alert(err);
 });
 
+//preload the personSegmentation from prediction to speed up process
 var loaded = personSegmentation.load();
 
-async function getIndex(segmentation) {
+// mask the video feed based on segmentation data
+async function maskVideo(segmentation) {
     ctx1.drawImage(video, 0, 0, videoWidth, videoHeight);
 
     let frame = ctx1.getImageData(0, 0, videoWidth, videoHeight);
     let l = frame.data.length/4;
-    //console.log(l)
     for (let i = 0; i < l; i++) {
-        //console.log(segmentation[i])
         if (segmentation[i] == 0) {
-            //console.log(segmentation[i])
             frame.data[i*4] = 255;
             frame.data[i*4 + 1] = 255;
             frame.data[i*4 + 2] = 255;
-            //ctx2.putImageData(ctx1.getImageData(w,h,1, 1), w , h)
         }
     }
-    // let l = frame.data.length / 4;
-    //
-    // for (let i = 0; i < l; i++) {
-    //   let r = frame.data[i * 4 + 0];
-    //   let g = frame.data[i * 4 + 1];
-    //   let b = frame.data[i * 4 + 2];
-    //   if (g > 10 && r > 10 && b < 43)
-    //     frame.data[i * 4 + 3] = 0;
-    // }
     ctx2.putImageData(frame, 0, 0);
-    //     };
-    // };
-    // counter = 1;
-    // console.log("loaded index");
-    // //console.log(indexVideoImage);
-    // var indexLength = 307200;
-    // console.log(indexLength);
-    // console.log(segmentation)
-    // for (let i = 0; i < indexLength; i++ ) {
-    //     if (segmentation[i] = 1) {
-    //         console.log(segmentation[i]);
-    //         //ctx.putImageData(ctx.getImageData(w,h,1, 1), w , h)
-    //     } else {
-    //         console.log('no');
-    //     }
-    // };
 };
 
-// // playing with the values
-// //var video = {width: 10, height: 10}
-// async function cutAvatar() {
-//     console.log(segmentation);
-//     // indexed.then(function(whatever) {
-//     //     return addImageToCanvas();
-//     // });
-//     // console.log(indexVideoImage[1000]);
-//     // console.log(indexVideoImage[1000][0]);
-//     //ctx.putImageData(ctx.getImageData(0,0,videoWidth, videoHeight), 0 , 0)
-//             // indexVideoImage[index].width = w;
-//             // indexVideoImage[index].height = h;
-//             //console.log(segmentation[index]);
-//             // if (segmentation[index] == 1) {
-//             //     console.log('yes');
-//             //     ctx.putImageData(ctx.getImageData(w,h,1, 1), w , h)
-//     //     };
-//     // };
-// };
-//             // if (segmentation[index] == 0) {
-//             //     ctx.putImageData()
-//             // }
-//     // let new_segments = segmentation;
-//     // for (let i=0; i < segmentation.length; i++) {
-//     //     if (new_segments[i] == 1) {
-//     //       ctx.putImageData(ctx.getImageData(0,0,videoWidth, videoHeight), 0 , 0);
-//     //   };
-//     // };
-//     // if(1) ctx.getImageData(w,h,1,1);
-//     //     };
-//     // };
-// //};
-
+// main function in cascade-mode
 async function runModel() {
     loaded.then(function(net){
       return net.estimatePersonSegmentation(video, flipHorizontal, 8, segmentationThreshold)
   }).then(function(segmentation){
-      getIndex(segmentation);
-      //console.log(segmentation);
-      //personSegmentation.drawBodyMaskOnCanvas(
-             //video, segmentation, canvas);
-      // display alternate image
-      // let new_segments = segmentation;
-      // for (let i=0; i < new_segments.length; i++) {
-      //     if (new_segments[i] == 1) {
-      //       new_segments[i] = 255;
-      //   };
-      // }
-      //console.log(new_segments);
-      //console.log(base64)
-      // img = new Image();
-      // //img.src = "data:image/png;base64," + base64;
-      // console.log(img)
-      // var ctx = canvas.getContext('2d');
-      // function drawSegments () {
-      //   ctx.putImageData(new_segments, 0, 0);
-      //   ctx.drawImage(img, 0, 0)
-      //   //img.style.display = 'none';
-      // };
-      // drawSegments()
+      maskVideo(segmentation);
+      // loops the function in a browser-sustainable way
       requestAnimationFrame(runModel);
     });
 }
